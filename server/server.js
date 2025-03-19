f065afc616f3ce71ad41653812092484b5801339
Abishek <abhisofttech360@gmail.com>
2025-03-19 07:02:49 +0530

    added Create User API

--- server/server.js
+++ server/server.js
@@ -4,9 +4,12 @@ import pg from 'pg';
 
 const app = express();
 dotenv.config();
+app.use(express.json())
 
 const PORT = process.env.PORT || 3001;
 const {Client} = pg;
+
+
 
 const client = new Client({
     user: process.env.POSTGRESQL_USERNAME,
@@ -18,8 +21,28 @@ const client = new Client({
 
 client.connect().then(()=>{
     console.log("DB Connected Successfully");
-})
+}).catch((err)=>{
+    console.log(`Error connecting the DB:${err}`)
+}
+)
 
+app.post('/', async(req,res)=>{
+const {name , age}=req.body;
+if(!name || !age){
+    return res.status(400).json({Error:"Name or Age is no define"});
+}
+try{
+    const create_query=`INSERT INTO user_info (name , age) VALUES ($1 , $2) RETURNING *`
+    const result=await client.query(create_query,[name,age]);
+    console.log("result",result)
+    res.send(result);
+}catch(err){
+    console.log("Error in creating User",err)
+    return res.status(400).json({Message:"Error Creating User",Error:err});
+}
+
+
+})
 
 
 
